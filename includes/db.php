f8c748b6234f274d95f7dcbfcee73431f0d877ca
Alex Sevesque <alex.sevesque@ecoles-wis.net>
2019-12-09 16:45:56 +0100

    final

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,14 @@
 <?php
-<<<<<<< Updated upstream
-require_once 'env.php';
+ob_start(); //Turns on output buffering
+session_start();
 
+$timezone = date_default_timezone_set("Europe/Paris");
 
-=======
+$con = mysqli_connect("localhost", "root", "", "mspr"); //Connection variable
 
-
-$con = mysqli_connect("localhost", "root","", "mspr");
-
-if(mysqli_connect_errno()){
-    echo "Failed to connect : "  . mysqli_connect_errno();
+if(mysqli_connect_errno())
+{
+    echo "Failed to connect: " . mysqli_connect_errno();
 }
 
->>>>>>> Stashed changes
 ?>
